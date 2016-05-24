64020236ae7e9a93eea17a0fc2edae4f0d18a98d
MelissaLozada <melissalozada56@gmail.com>
2016-05-24 13:00:17 +0800

    Portfolio

--- js/app.js
+++ js/app.js
@@ -1,56 +1,46 @@
-<<<<<<< Updated upstream
 $(document).ready(function(){
-	$(".button-projects").click(function(event){
-		event.preventDefault();
-		var goTopPosition = $(".projects").offset().top;
-		$("html, body").animate({ scrollTop:goTopPosition }, 1000, "easeInOutQuart");
+
+
 	});
-
-	$(".button-home").click(goToSplash);
-	$(".button-about").click(goToAbout);
-	$(".button-contact").click(goToContact);
-});
-
-function goToSplash (event) {
-	event.preventDefault();
-	var goTopPosition = $(".splash").offset().top;
-	$("html, body").animate({ scrollTop:goTopPosition }, 1000, "easeInOutQuart");
-}
-
-function goToAbout (event) {
-	event.preventDefault();
-	var goTopPosition = $(".about").offset().top;
-	$("html, body").animate({ scrollTop:goTopPosition }, 1000, "easeInOutQuart");
-}
-function goToContact (event) {
-	event.preventDefault();
-	var goTopPosition = $(".contact").offset().top;
-	$("html, body").animate({ scrollTop:goTopPosition }, 1000, "easeInOutQuart");
-}
-
-
-
-
-
-
-
-
-
-=======
-$(document).ready(function () {
 	
-	window.sr = ScrollReveal( { reset: true });
-	sr.reveal('.splash');
-	sr.reveal('.header', { duration:2500});
-	sr.reveal('.description', { duration: 3200});
-	sr.reveal('projects .header');
-	sr.reveal('projects .projects-item', 
-		{   duration:2000,
-			origin:"top",
-		    scale: 1
-		 }
-		)
+	
 
-});
 
->>>>>>> Stashed changes
+	$(".button-home").click(goTohome);
+	$(".button-projects").click(goToprojects);
+	$(".button-about").click(goToabout);
+	$(".button-contacts").click(goTocontact);
+
+
+
+
+
+function goTohome (event){
+	event.preventDefault();
+	var goTopposition = $(".chocolate").offset().top;
+		$("html,body").animate({scrollTop:goTopposition},1000,"easeInOutQuart");
+
+}
+
+function goToabout (event){
+	event.preventDefault();
+	var goTopposition = $(".about").offset().top;
+		$("html,body").animate({scrollTop:goTopposition},1000,"easeInOutQuart");
+
+}
+
+function goToprojects (event){
+	event.preventDefault();
+	var goTopposition = $(".projects").offset().top;
+		$("html,body").animate({scrollTop:goTopposition},1000,"easeInOutQuart");
+
+}
+
+function goTocontacts (event){
+	event.preventDefault();
+	var goTopposition = $(".contact").offset().top;
+		$("html,body").animate({scrollTop:goTopposition},1000,"easeInOutQuart");
+}
+
+
+
